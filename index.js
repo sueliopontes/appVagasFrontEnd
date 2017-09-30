d56ceb8ea6af9294e058acc3f3eaf6655ed74064
Suelio <suelio.pontes@hotmail.com>
2017-09-29 21:22:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var app = express();
+var cons = require('consolidate');
 
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/public/vagas'));
 
-app.get('/', express.static(__dirname + '/public/vagas'));
+app.engine('html', cons.swig);
+app.set('view engine','html');
+app.set('views',__dirname+'/public/vagas');
+
+app.get('/',function(req,res){
+  res.render('index')});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
