f37aaeb29ca74f1598cffa8149fc00155c3091c5
Suelio <suelio.pontes@hotmail.com>
2017-09-29 21:02:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public/vagas'));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+app.get('/', express.static(__dirname + '/public/vagas'));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
