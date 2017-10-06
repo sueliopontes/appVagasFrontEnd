054a16e00ad99a4975d438eaccb100e695b0bffa
Suélio <suelio.pontes@hotmail.com>
2017-10-05 21:02:02 -0300

    Corrigido injeção de dependência

--- public/vagas/js/controllers/inscrito/inscritoNewCtrl.js
+++ public/vagas/js/controllers/inscrito/inscritoNewCtrl.js
@@ -1,4 +1,6 @@
-angular.module("escola").controller("inscritoNewCtrl", function ($scope,inscritoAPI,$location) {	
+angular.module("escola").controller("inscritoNewCtrl", function ($scope,inscritoAPI,$location,alunos,turmas) {	
+    $scope.alunos=alunos.data;
+    $scope.turmas=turmas.data;
     
         $scope.adicionarInscrito = function (inscrito) {		
             inscritoAPI.saveInscrito(inscrito).success(function (data) {
