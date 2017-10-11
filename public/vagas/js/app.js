f222e72273b329928d6e8406b24f1c30d898ff36
Suélio <suelio.pontes@hotmail.com>
2017-10-11 17:40:11 -0300

    Corrigido app.js

--- public/vagas/js/app.js
+++ public/vagas/js/app.js
@@ -1,9 +1,1 @@
 angular.module("escola", ["ngMessages", "ui", "ngRoute"]);
-
-app.config(['$httpProvider', function ($httpProvider) {
-	  //Reset headers to avoid OPTIONS request (aka preflight)
-	  $httpProvider.defaults.headers.common = {};
-	  $httpProvider.defaults.headers.post = {};
-	  $httpProvider.defaults.headers.put = {};
-	  $httpProvider.defaults.headers.patch = {};
-	}]);
