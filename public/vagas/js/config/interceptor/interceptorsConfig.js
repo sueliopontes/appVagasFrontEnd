02bb76d12245098ce87b38f31dc94df19a18d03d
Suélio <suelio.pontes@hotmail.com>
2017-10-25 21:12:21 -0300

    Desativando interceptor https

--- public/vagas/js/config/interceptor/interceptorsConfig.js
+++ public/vagas/js/config/interceptor/interceptorsConfig.js
@@ -1,6 +1,6 @@
 angular.module("escola").config(function ($httpProvider) {	
 	//$httpProvider.interceptors.push("timestampInterceptor");
 	//$httpProvider.interceptors.push("errorInterceptor");
-	$httpProvider.interceptors.push("httpsInterceptor");
+	//$httpProvider.interceptors.push("httpsInterceptor");
 	
 });
