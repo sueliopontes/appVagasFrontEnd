0f8bfefcbda1d0fac0ca13780aa1e56caf22bec9
Suélio <suelio.pontes@hotmail.com>
2017-10-26 14:53:29 -0200

    Adicionado campos na tela de inscritoNew

--- vagas/js/controllers/public/publicCtrl.js
+++ vagas/js/controllers/public/publicCtrl.js
@@ -1,11 +1,12 @@
 angular.module("escola").controller("publicCtrl", function ($scope, inscritos,escolas,inscritoAPI) {	
 	$scope.inscritos = inscritos.data;	
-	$scope.escolas=escolas.data;
+	$scope.escolas = escolas.data;
 
 	$scope.carregarInscritos = function (escola) {
 		inscritoAPI.getInscritosByEscola(escola).success(function (data) {
 		console.log(escola);
-			$scope.escolas = escolas.data;
+		console.log(data);
+			$scope.inscritos = data;
 
 		}).error(function(data,status){							
 			$location.path("/error");			
