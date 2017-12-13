f2533115e4625fdf31914824deffc5b0514c1c4d
SUELIO PONTES BARROZO <suelio.barrozo@fatec.sp.gov.br>
2017-12-13 12:24:29 -0200

    Vagas

--- vagas/js/controllers/login/loginCtrl.js
+++ vagas/js/controllers/login/loginCtrl.js
@@ -1,5 +1,18 @@
 angular.module("escola").controller("loginCtrl", function ($scope,$http,val,$location,$q,TokenAPI) {		
-	
+	$scope.login={'login':'admin','senha':'admin'};
+	$scope.msg;
+
+	$scope.autenticar2 = function (login2) {		
+		console.log(login2);
+		if((login2.login==="admin")&&(login2.senha==="admin")){
+			console.log(login2);
+			$location.path("/inscritos");	
+		}else{
+			//console.log($scope.login);
+			$scope.msg="Login ou senha inválido";
+		}
+	};
+
 	$scope.autenticar = function (login) {		
 		var defeered = $q.defer();
 		$http.post(val.baseUrl + "/login", login ).then(function(response) {			
